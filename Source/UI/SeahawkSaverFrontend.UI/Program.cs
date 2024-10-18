7d9bd74e6feea6493839ba52cb786530ada163b1
TateHouse <tatemonroehouse@gmail.com>
2024-10-17 22:02:24 -0400

    Feature: Application entry point

--- Source/UI/SeahawkSaverFrontend.UI/Program.cs
+++ Source/UI/SeahawkSaverFrontend.UI/Program.cs
@@ -1,34 +1,13 @@
-using SeahawkSaverFrontend.UI.Components;
-
 namespace SeahawkSaverFrontend.UI;
 public class Program
 {
-	public static void Main(string[] args)
+	public async static Task Main(string[] args)
 	{
-		var builder = WebApplication.CreateBuilder(args);
+		var webApplicationBuilder = WebApplication.CreateBuilder(args);
+		webApplicationBuilder.ConfigureServices();
 
-		// Add services to the container.
-		builder.Services.AddRazorComponents()
-			   .AddInteractiveServerComponents();
-
-		var app = builder.Build();
-
-		// Configure the HTTP request pipeline.
-		if (!app.Environment.IsDevelopment())
-		{
-			app.UseExceptionHandler("/Error");
-			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-			app.UseHsts();
-		}
-
-		app.UseHttpsRedirection();
-
-		app.UseStaticFiles();
-		app.UseAntiforgery();
-
-		app.MapRazorComponents<App>()
-		   .AddInteractiveServerRenderMode();
-
-		app.Run();
+		var application = webApplicationBuilder.Build();
+		application.ConfigureMiddleware();
+		await application.RunAsync();
 	}
 }
---
+++ Source/UI/SeahawkSaverFrontend.UI/StartupExtensions.cs
@@ -0,0 +1,67 @@
+﻿namespace SeahawkSaverFrontend.UI;
+using SeahawkSaverFrontend.Application.UnitTest;
+using SeahawkSaverFrontend.UI.Components;
+
+/**
+ * <summary>
+ * Extension methods for configuring the application's services and middleware.
+ * </summary>
+ */
+public static class StartupExtensions
+{
+	/**
+	 * <summary>
+	 * An extension method for <see cref="WebApplicationBuilder"/> to configure the application's services.
+	 * </summary>
+	 * <returns>The <see cref="WebApplicationBuilder"/> instance.</returns>
+	 */
+	public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+	{
+		builder.Configuration.Sources.Clear();
+		builder.Configuration.AddJsonFile("AppSettings.json", false, true);
+		builder.Configuration.AddJsonFile($"AppSettings.{builder.Environment.EnvironmentName}.json", true, true);
+		builder.Configuration.AddUserSecrets<Program>();
+		builder.Configuration.AddEnvironmentVariables();
+
+		builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(configureOptions =>
+		{
+			configureOptions.SerializerOptions.PropertyNamingPolicy = null;
+		});
+
+		builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(configureOptions =>
+		{
+			configureOptions.JsonSerializerOptions.PropertyNamingPolicy = null;
+		});
+
+		builder.Services
+			   .AddRazorComponents()
+			   .AddInteractiveServerComponents();
+
+		builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+		builder.Services.RegisterApplicationServices();
+
+		return builder;
+	}
+
+	/**
+	 * <summary>
+	 * An extension method for <see cref="WebApplication"/> to configure the application's middleware.
+	 * </summary>
+	 * <returns>The <see cref="WebApplication"/> instance.</returns>
+	 */
+	public static WebApplication ConfigureMiddleware(this WebApplication application)
+	{
+		if (application.Environment.IsDevelopment() == false)
+		{
+			application.UseHsts();
+		}
+
+		application.UseHttpsRedirection();
+		application.UseStaticFiles();
+		application.UseAntiforgery();
+		application.MapRazorComponents<App>()
+				   .AddInteractiveServerRenderMode();
+
+		return application;
+	}
+}
