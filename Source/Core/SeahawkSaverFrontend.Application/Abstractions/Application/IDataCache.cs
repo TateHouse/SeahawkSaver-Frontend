fa99f5425098334388e4f4973c7fd59c0685963d
TateHouse <tatemonroehouse@gmail.com>
2024-10-30 00:00:56 -0400

    Build: Remove projects

--- Source/Core/SeahawkSaverFrontend.Application/Abstractions/Application/IDataCache.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SeahawkSaverFrontend.Application.Abstractions.Application;
-public interface IDataCache<TData>
-{
-	public TData? Data { get; }
-
-	public void UpdateCache(TData data);
-}
--- Source/Core/SeahawkSaverFrontend.Application/Abstractions/Application/Services/ILoginService.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SeahawkSaverFrontend.Application.Abstractions.Application.Services;
-public interface ILoginService
-{
-	public Task<bool> LoginAsync(string email, string password);
-}
