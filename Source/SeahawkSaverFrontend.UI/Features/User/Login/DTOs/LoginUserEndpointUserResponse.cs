6cdf64b81f09f83d0c0a464ca089d41b12b994ae
TateHouse <tatemonroehouse@gmail.com>
2024-11-03 18:09:52 -0500

    Feature: Login admin
    Admins use the same login process as regular users.

--- Source/SeahawkSaverFrontend.UI/Features/User/Login/DTOs/LoginUserEndpointUserResponse.cs
+++ Source/SeahawkSaverFrontend.UI/Features/User/Login/DTOs/LoginUserEndpointUserResponse.cs
@@ -10,4 +10,5 @@ public class LoginUserEndpointUserResponse
 	public required string Email { get; init; }
 	public required string FirstName { get; init; }
 	public required string LastName { get; init; }
+	public required bool IsAdmin { get; init; }
 }
