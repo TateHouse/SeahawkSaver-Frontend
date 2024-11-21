a575e3ecd2b7d6a230f1e4ca26551f8923c5a885
TateHouse <tatemonroehouse@gmail.com>
2024-11-20 20:06:31 -0500

    Feature: Deactivate user component

--- Source/SeahawkSaverFrontend.UI/Features/User/Manage/Components/UserManagementComponent.razor.cs
+++ Source/SeahawkSaverFrontend.UI/Features/User/Manage/Components/UserManagementComponent.razor.cs
@@ -6,8 +6,9 @@ using SeahawkSaverFrontend.UI.Features.User.Manage.DTOs;
 public partial class UserManagementComponent : ComponentBase
 {
 	private UserEntryModel model;
+	private List<UserEntryModel> users = new List<UserEntryModel>();
 
-	protected override void OnInitialized()
+	protected override async Task OnInitializedAsync()
 	{
 		model = new UserEntryModel
 		{
@@ -15,8 +16,26 @@ public partial class UserManagementComponent : ComponentBase
 			Email = DataCache.User.Email,
 			FirstName = DataCache.User.FirstName,
 			LastName = DataCache.User.LastName,
+			IsAdmin = DataCache.User.IsAdmin,
+			IsActive = DataCache.User.IsActive,
 			IsEditable = false
 		};
+
+		if (DataCache.User.IsAdmin)
+		{
+			users = (await UserService.GetUsersAsync())
+					.Select(user => new UserEntryModel
+					{
+						UserId = user.UserId,
+						Email = user.Email,
+						FirstName = user.FirstName,
+						LastName = user.LastName,
+						IsAdmin = user.IsAdmin,
+						IsActive = user.IsActive,
+						IsEditable = false
+					})
+					.ToList();
+		}
 
 		StateHasChanged();
 	}
--- Source/SeahawkSaverFrontend.UI/Features/User/Services/IUserService.cs
+++ Source/SeahawkSaverFrontend.UI/Features/User/Services/IUserService.cs
@@ -3,5 +3,6 @@ using SeahawkSaverFrontend.UI.Features.User.DTOs;
 
 public interface IUserService
 {
+	public Task<IEnumerable<UserModel>> GetUsersAsync();
 	public Task<bool> UpdateUserAsync(UserModel userModel);
 }
