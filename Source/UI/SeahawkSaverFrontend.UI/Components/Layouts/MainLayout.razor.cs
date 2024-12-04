218cafa8a38f8f97c4e13c49ee5a7d93d7866ea0
TateHouse <tatemonroehouse@gmail.com>
2024-12-03 21:18:39 -0500

    Feature: Seahawk color theme

--- Source/UI/SeahawkSaverFrontend.UI/Components/Layouts/MainLayout.razor.cs
+++ Source/UI/SeahawkSaverFrontend.UI/Components/Layouts/MainLayout.razor.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
 
 namespace SeahawkSaverFrontend.UI.Components.Layouts;
+using MudBlazor;
+using SeahawkSaverFrontend.UI.Themes;
+
 public partial class MainLayout : LayoutComponentBase
 {
-
+	private readonly MudTheme mudTheme = new SeahawkTheme();
 }
