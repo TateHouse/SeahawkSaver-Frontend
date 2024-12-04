93ce831c3d77918db5a8b9aab62f7b5d51cdbdf3
TateHouse <tatemonroehouse@gmail.com>
2024-12-03 22:07:57 -0500

    Feature: In-memory authentication cache

---
+++ Source/Core/SeahawkSaverFrontend.Application/Abstractions/Caching/IAuthenticationCache.cs
@@ -0,0 +1,11 @@
+﻿namespace SeahawkSaverFrontend.Application.Abstractions.Caching;
+/**
+ * <summary>
+ * An interface for a data cache containing authentication related data.
+ * </summary>
+ */
+public interface IAuthenticationCache
+{
+	public event Action? OnChange;
+	public string Token { get; set; }
+}
---
+++ Source/Core/SeahawkSaverFrontend.Application/Features/Caching/InMemoryAuthenticationCache.cs
@@ -0,0 +1,29 @@
+﻿namespace SeahawkSaverFrontend.Application.Features.Caching;
+using SeahawkSaverFrontend.Application.Abstractions.Caching;
+
+/**
+ * <summary>
+ * An in-memory data cache containing authentication related data.
+ * </summary>
+ */
+public sealed class InMemoryAuthenticationCache : IAuthenticationCache
+{
+	private string token = string.Empty;
+
+	public event Action? OnChange;
+
+	public string Token
+	{
+		get
+		{
+			return token;
+		}
+
+		set
+		{
+			token = value;
+			OnChange?.Invoke();
+
+		}
+	}
+}
