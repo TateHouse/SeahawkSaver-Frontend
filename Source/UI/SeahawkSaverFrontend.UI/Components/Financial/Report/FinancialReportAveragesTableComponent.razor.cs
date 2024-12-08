81d1e335baf194ebcda0edf795c9d5bf9a2559bb
TateHouse <tatemonroehouse@gmail.com>
2024-12-08 02:47:48 -0500

    Feature: Monthly averages table

---
+++ Source/UI/SeahawkSaverFrontend.UI/Components/Financial/Report/FinancialReportAveragesTableComponent.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SeahawkSaverFrontend.UI.Components.Financial.Report;
+using SeahawkSaverFrontend.Domain.Models.Financial;
+
+public partial class FinancialReportAveragesTableComponent : ComponentBase
+{
+	private sealed record TableRow
+	{
+		public required decimal MonthlyAverage { get; init; }
+		public required FinancialModelType Type { get; init; }
+	}
+
+	private IEnumerable<TableRow> financialModelMonthlyAverages = null!;
+
+	protected override void OnInitialized()
+	{
+		financialModelMonthlyAverages = new List<TableRow>
+		{
+			new TableRow
+			{
+				MonthlyAverage = FinancialReportManager.AverageFinancialModelsPerMonth.AverageDebtAmount,
+				Type = FinancialModelType.Debt
+			},
+			new TableRow
+			{
+				MonthlyAverage = FinancialReportManager.AverageFinancialModelsPerMonth.AverageExpenseAmount,
+				Type = FinancialModelType.Expense
+			},
+			new TableRow
+			{
+				MonthlyAverage = FinancialReportManager.AverageFinancialModelsPerMonth.AverageIncomeAmount,
+				Type = FinancialModelType.Income
+			},
+			new TableRow
+			{
+				MonthlyAverage = FinancialReportManager.AverageFinancialModelsPerMonth.AverageSavingAmount,
+				Type = FinancialModelType.Saving
+			},
+			new TableRow
+			{
+				MonthlyAverage = FinancialReportManager.AverageFinancialModelsPerMonth.AverageSubscriptionAmount,
+				Type = FinancialModelType.Subscription
+			}
+		};
+	}
+}
--- Source/UI/SeahawkSaverFrontend.UI/Components/Financial/Report/FinancialReportTotalsTableComponent.razor.cs
+++ Source/UI/SeahawkSaverFrontend.UI/Components/Financial/Report/FinancialReportTotalsTableComponent.razor.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 
 namespace SeahawkSaverFrontend.UI.Components.Financial.Report;
-using SeahawkSaverFrontend.UI.Components.Financial.Report.DTOs;
-
 public partial class FinancialReportTotalsTableComponent : ComponentBase
 {
-	[Parameter]
-	public IEnumerable<FinancialModelTotal> FinancialModelTotals { get; set; }
+
 }
